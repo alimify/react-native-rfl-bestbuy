0242e9e64127fa9952a5659fac7c7baa4b93e25b
Alim <alim@tritiyo.com>
2020-02-23 09:11:35 +0600

    Working on account screen

--- screens/user/index.tsx
+++ screens/user/index.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import { withNavigation } from 'react-navigation'
 import { inject, observer } from "mobx-react";
+import DefaultStyles from '../../constants/DefaultStyles'
 
 const UserIndex = props => {
 
@@ -21,18 +22,48 @@ const UserIndex = props => {
 
 
     return (
-      <View>
-        <Text>
-          {JSON.stringify(user.ACCOUNT.user.name)}
-          {/* {JSON.stringify(user.ACCESS_TOKEN)} */}
-        </Text>
-        <Button
-          title="Go Category"
-          onPress={() => {
-            props.navigation.navigate("Category");
-          }}
-        />
-      </View>
+      <ScrollView>
+       
+        <View style={DefaultStyles.flexContainer}>
+          <View>
+            <Text>Total Orders</Text>
+            <Text>{user.ACCOUNT.totalOrders}</Text>
+          </View>
+
+          <View>
+            <Text>Pending Orders</Text>
+            <Text>{user.ACCOUNT.totalPending}</Text>
+          </View>
+
+          <View>
+            <Text>Complete Orders</Text>
+            <Text>{user.ACCOUNT.totalComplete}</Text>
+          </View>
+        </View>
+
+        <View style={DefaultStyles.flexContainer}>
+          <View>
+            <Text>Total Purchase</Text>
+            <Text>{user.ACCOUNT.totalSpend}</Text>
+          </View>
+
+          <View>
+            <Text>Earned Points</Text>
+            <Text>{user.ACCOUNT.user.reward_points}</Text>
+          </View>
+
+        </View>
+      
+        <View>
+          <Text>Account Details</Text>
+          <View style={DefaultStyles.flexContainer}>
+            <Text>Full Name:</Text>
+            <Text>Admin</Text>
+          </View>
+        </View>
+
+
+      </ScrollView>
     );
 
 
